f1025dfef13838819f874b0daa022ca5cbe5ad93
Ashley Kotian <ashleykotian@gmail.com>
2016-02-01 23:25:39 -0800

    Model migration and basic serializer

--- posts/models.py
+++ posts/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    short_description = models.CharField(
+       max_length=500, blank=True)
+    # long_description
+    # repo_url
+    # author
+    # interested_users
+    # active_users
+    created = models.DateTimeField(
+        'date created', auto_now_add=True)
