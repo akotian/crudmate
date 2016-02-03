ae29a14e0792303eb8ef809c046377cd9d54f2c4
Ashley Kotian <ashleykotian@gmail.com>
2016-02-02 22:57:36 -0800

    Loading posts from server API

--- crudmate/static/js/posts.js
+++ crudmate/static/js/posts.js
@@ -34,8 +34,8 @@ var PostList = React.createClass({
       return (
         <Post title={post.title} description={post.description} key={post.id} />
       );
-
     });
+
     return (
       <div className="postList">
         {postNodes}
@@ -45,18 +45,38 @@ var PostList = React.createClass({
 });
 
 var PostBox = React.createClass({
+  loadPostsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+        }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadPostsFromServer();
+    setInterval(this.loadPostsFromServer, this.props.pollInterval)
+  },
   render: function() {
     return (
       <div className="postBox">
         <PostForm />
         <h1>Posts</h1>
-        <PostList data={this.props.data} />
+        <PostList data={this.state.data} />
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <PostBox data={posts} />,
+  <PostBox url="/api/posts/" pollInterval={2000} />,
   document.getElementById('content')
 );
