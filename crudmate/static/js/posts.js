acfa8b91697fe7f6b705b81ee49006da5289553b
Ashley Kotian <ashleykotian@gmail.com>
2016-02-03 22:50:09 -0800

    Able to submit a post

--- crudmate/static/js/posts.js
+++ crudmate/static/js/posts.js
@@ -1,8 +1,3 @@
-var posts = [
-  {'id': 1, 'title': 'My first post', 'description': 'My first description'},
-  {'id': 2, 'title': 'My second post', 'description': 'My second description'}
-]
-
 var Post= React.createClass({
   render: function() {
     return (
@@ -10,8 +5,8 @@ var Post= React.createClass({
         <span className="postTitle">
           {this.props.title}
         </span>
-        <span className="postDesciption">
-          {this.props.description}
+        <span className="postShortDescription">
+          {this.props.short_description}
         </span>
       </div>
     );
@@ -19,10 +14,37 @@ var Post= React.createClass({
 });
 
 var PostForm = React.createClass({
+  getInitialState: function() {
+    return {title: '', short_description: ''}
+
+  },
+  handleTitleChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleShortDescriptionChange: function(e) {
+    this.setState({short_description: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = this.state.title.trim();
+    var short_description = this.state.short_description.trim();
+    if (!title) {
+      return;
+    }
+    this.props.onPostSubmit({title: title, short_description: short_description});
+    this.setState({title: '', short_description: ''});
+
+  },
   render: function() {
     return (
       <div className="postForm">
-      Wassa, I am a post form!
+      <form className="postForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Title"
+          value={this.state.title} onChange={this.handleTitleChange} />
+        <input type="text" placeholder="Short Description"
+          value={this.state.short_description} onChange={this.handleShortDescriptionChange} />
+        <input type="submit" value="Post" />
+      </form>
       </div>
     );
   }
@@ -32,7 +54,7 @@ var PostList = React.createClass({
   render: function() {
     var postNodes = this.props.data.map(function(post) {
       return (
-        <Post title={post.title} description={post.description} key={post.id} />
+        <Post title={post.title} short_description={post.short_description} key={post.id} />
       );
     });
 
@@ -65,10 +87,29 @@ var PostBox = React.createClass({
     this.loadPostsFromServer();
     setInterval(this.loadPostsFromServer, this.props.pollInterval)
   },
+  handlePostSubmit: function(post) {
+    var posts = this.state.data;
+    post.id = Date.now();
+    var newPosts = posts.concat([post]);
+    this.setState({data: newPosts});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: post,
+      success: function(newPost) {
+        this.setState({data: posts.concat(newPost)});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data: posts});
+        console.error(this.props.url, status, err.toString());
+        }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="postBox">
-        <PostForm />
+        <PostForm onPostSubmit={this.handlePostSubmit} />
         <h1>Posts</h1>
         <PostList data={this.state.data} />
       </div>
